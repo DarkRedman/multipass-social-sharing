c55a986d35f6c0ab8d9d9f0a5825190fb643c0af
DarkRedman <contact@darkredman.fr>
2022-01-26 16:10:10 +0100

    Updated auth system

--- views/login.py
+++ views/login.py
@@ -9,17 +9,17 @@ logout_blueprint = Blueprint('logout', __name__,)
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    if request.method == 'POST':
+    elif request.method == 'POST':
         email = request.form["email"]
         password = request.form["password"]
         
         if database.validate_user(email, password):
-            return render_template("loginFailed.html")
-        else:
             response = make_response(redirect(current_app.config['BASEURL'] + 'dashboard'))
             response.set_cookie('email', email)
             response.set_cookie('userID', current_app.config['SECRET_KEY'] + email) # It's risky to use sole SECRET_KEY as cookie in production
             return response
+        else:
+            return render_template("loginFailed.html")
 
 
 @logout_blueprint.route('/logout')
