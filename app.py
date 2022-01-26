08d220b43407a26e4487cf14f94f1d124b110f38
DarkRedman <contact@darkredman.fr>
2022-01-26 09:47:35 +0100

    Enhance settings management

--- app.py
+++ app.py
@@ -10,22 +10,12 @@ from views.tasks import tasks_blueprint, basic_task_create_blueprint,\
 from views.userManagement import user_account_blueprint, user_account_create_blueprint, user_account_edit_blueprint
 from flask import Flask
 
-from settings import UPLOAD_FOLDER, AUTHORIZED_FILES_TYPE, SECRET
-import settings
 
 # get HTTPS with Talisman
 from flask_talisman import Talisman
 
 app = Flask(__name__, static_folder='static')
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['ALLOWED_EXTENSIONS'] = AUTHORIZED_FILES_TYPE
-
-app.config['UPLOAD_FOLDER'] = '/static/images'
-app.config['SESSION_COOKIE_NAME'] = 'userSession'
-app.config['SECRET_KEY'] = secrets.token_urlsafe(64)
-
-settings.SECRET = str(app.config.get('SECRET_KEY'))
+app.config.from_object('settings.DebugConfig')
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(login_blueprint)
--- settings.py
+++ settings.py
@@ -1,5 +1,22 @@
-BASEURL = 'https://multi-pass.org/'
-SECRET = ""
-UPLOAD_FOLDER = "static/images"
-AUTHORIZED_FILES_TYPE = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
-DATABASE = 'multipass'
+import secrets
+
+class DebugConfig(object):
+    ENV = 'development'
+	BASEURL = 'http://localhost:5000/'
+	UPLOAD_FOLDER = '/static/images/'
+    SECRET_KEY = 'do-i-really-need-this'
+    DATABASE = 'multipass'
+    DATABASE_PATH = 'db.sqlite'
+    AUTHORIZED_FILES_TYPE = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
+    SESSION_COOKIE_NAME = 'userSession'
+
+class ProductionConfig(Config):
+    ENV = 'production'
+	BASEURL = 'https://multi-pass.org/'
+	UPLOAD_FOLDER = '/static/images'
+	SECRET_KEY = secrets.token_urlsafe(64)
+    DB_HOST = 'my.production.database' # not a docker link
+    DATABASE = 'multipass'
+    DATABASE_PATH = '/srv/multipass.db'
+    AUTHORIZED_FILES_TYPE = {'png', 'jpg', 'jpeg', 'gif', 'svg'}
+    SESSION_COOKIE_NAME = 'userSession'
