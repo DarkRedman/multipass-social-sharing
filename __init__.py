9640bef36d6a66c5f2b6b477bc4431acde9ec588
DarkRedman <contact@darkredman.fr>
2022-01-27 18:14:04 +0100

    Update config.py and __init__.py

---
+++ __init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask, render_template
+
+
+def create_app(test_config=True):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config:
+        app.config.from_object('multipass.config.DebugConfig')
+    else:
+        app.config.from_object('multipass.config.ProductionConfig')
+
+    if app.config['ENV'] == 'development':
+        app.instance_path = app.root_path
+        app.config['DATABASE_PATH'] = os.path.join(app.instance_path, app.config['DATABASE_PATH'])
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Homepage
+    @app.route('/')
+    def index():
+        return render_template('home.html')
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import tasks
+    app.register_blueprint(tasks.bp)
+
+    return app
---
+++ auth.py
@@ -0,0 +1,90 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from multipass.db import query_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form["email"]
+        password = request.form["password"]
+        error = None
+
+        user_exist = query_db("SELECT * FROM users WHERE email=?", (email,), one=True)
+
+        if not email:
+            error = 'Email is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif user_exist:
+            error = 'User {} is already registered.'.format(email)        
+
+        if error is None:
+            encrypted_password = generate_password_hash(password)
+            query_db("INSERT INTO users(email, password) VALUES(?,?)", (email, encrypted_password))
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form["email"]
+        password = request.form["password"]
+        error = None
+        user = query_db('SELECT * FROM users WHERE email = ?', (email,), one=True)
+
+        if user is None or not check_password_hash(user['password'], password):
+            error = 'Incorrect email or password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = email
+            return redirect(url_for('index'))
+        
+        flash(error)
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = query_db('SELECT * FROM users WHERE email = ?', (user_id,), one=True)
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+# @bp.route('/<username>/account', methods=['GET'])
+# def route_to_account(username):
+#     email = database.get_user_email(username)
+#     return render_template('userAccount.html', baseurl=current_app.config['BASEURL'], username=username, email=email)
+
+
+# @bp.route('/<username>/edit', methods=['POST'])
+# def route_to_edit_account(username):
+#     email = request.form["email"]
+#     password = request.form["password"]
+#     database.update_user(email, password)
+#     return render_template('login.html', baseurl=current_app.config['BASEURL'], validation_message="user account edited")
