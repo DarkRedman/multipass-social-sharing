c885504705b098aa7a01a448982803d3d20957cc
DarkRedman <contact@darkredman.fr>
2022-01-27 13:25:51 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,21 +1,59 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  email TEXT NOT NULL UNIQUE,
-  password TEXT NOT NULL
-);
+  email VARCHAR(30) NOT NULL, 
+  password VARCHAR(100) NOT NULL, 
+  PRIMARY KEY (email), 
+  UNIQUE (email), 
+  UNIQUE (password)
+)
 
-DROP TABLE IF EXISTS basic_tasks;
+DROP TABLE IF EXISTS basicTask;
 
-CREATE TABLE basic_tasks (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_id INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  date TIMESTAMP NOT NULL,
-  repetition INTEGER NOT NULL,
-  days INTEGER NOT NULL,
-  months INTEGER NOT NULL,
-  facebook BOOLEAN NOT NULL,
-  twitter BOOLEAN NOT NULL,
-  instagram BOOLEAN NOT NULL
-);
+CREATE TABLE "basicTask" (
+  "task_id" INTEGER NOT NULL UNIQUE,
+  "user_id" INTEGER NOT NULL,
+  "name"  VARCHAR NOT NULL,
+  "date"  VARCHAR NOT NULL,
+  "repetition"  VARCHAR NOT NULL,
+  "days"  VARCHAR NOT NULL,
+  "months"  VARCHAR NOT NULL,
+  UNIQUE("task_id"),
+  PRIMARY KEY("task_id" AUTOINCREMENT)
+)
+
+DROP TABLE IF EXISTS FacebookTask;
+
+CREATE TABLE "FacebookTask" (
+  task_id INTEGER NOT NULL, 
+  message VARCHAR NOT NULL, 
+  files VARCHAR NOT NULL, 
+  PRIMARY KEY (task_id), 
+  UNIQUE (task_id), 
+  UNIQUE (message), 
+  UNIQUE (files)
+)
+
+DROP TABLE IF EXISTS InstagramTask;
+
+CREATE TABLE "InstagramTask" (
+  task_id INTEGER NOT NULL, 
+  message VARCHAR NOT NULL, 
+  files VARCHAR NOT NULL, 
+  PRIMARY KEY (task_id), 
+  UNIQUE (task_id), 
+  UNIQUE (message), 
+  UNIQUE (files)
+)
+
+DROP TABLE IF EXISTS TwitterTask;
+
+CREATE TABLE "TwitterTask" (
+  task_id INTEGER NOT NULL, 
+  message VARCHAR NOT NULL, 
+  files VARCHAR NOT NULL, 
+  PRIMARY KEY (task_id), 
+  UNIQUE (task_id), 
+  UNIQUE (message), 
+  UNIQUE (files)
+)
