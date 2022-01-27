a90ebb024bb87ac9d7293b4881c2016c82464a2b
DarkRedman <contact@darkredman.fr>
2022-01-27 12:19:42 +0100

    Remove views folders, views are now in tasks.py and auth.py with their respective blueprints.

--- views/home.py
+++
@@ -1,8 +0,0 @@
-from flask import current_app, Blueprint, render_template
-
-home_blueprint = Blueprint('home', __name__,)
-
-
-@home_blueprint.route('/', methods=['GET'])
-def route_to_home():
-    return render_template('home.html', baseurl=current_app.config['BASEURL'])
